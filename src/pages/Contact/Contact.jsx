d6685db030770469dd97fa1bdaa1f5ddac69d302
israel <odejobiakin@gmail.com>
2023-09-24 03:33:24 +0100

    removed unused variables

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -29,7 +29,29 @@ const Contact = () => {
 
   return (
     <div className="contact">
-      <div className="contact-details"></div>
+      <div className="contact-details">
+        <h2>Get in touch</h2>
+        <p>
+          Contact <br />
+          Information
+        </p>
+        <p>
+          27,Alara Street <br />
+          Yaba 100012 <br />
+          Lagos State
+        </p>
+        <p>Call Us : 07067981819</p>
+        <p>
+          we are open from Monday-Friday <br />
+          08:00am - 05:00pm
+        </p>
+        <div className="social">
+          <p>Share on</p>
+          <div>
+            <img src={social_img} />
+          </div>
+        </div>
+      </div>
       <div className="contact-form">
         <div className="contact-form_title">
           <h2>Questions or need assistance? Let us know about it</h2>
